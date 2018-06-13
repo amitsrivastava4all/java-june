f1103bc96b68823276ed34eabd23c445cff63873
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-06-13 13:52:01 +0530

    Code

---
+++ Code/DataTypes.java
@@ -0,0 +1,18 @@
+ class DataTypes{
+public static strictfp void main(String args[]){
+String msg = "नमस्ते";
+//char msg[] = {'न','म','स','त'};
+float w = 100.20f;
+double f = 12.876543;
+int firstNumber = 100;  // Local Variables
+int secondNumber =200;
+int result;
+result = firstNumber + secondNumber;
+/*
+for(int i = 0; i<msg.length;i++){
+System.out.print(msg[i]);
+}*/
+System.out.println(msg);
+System.out.println("Sum is "+result);
+}
+}
