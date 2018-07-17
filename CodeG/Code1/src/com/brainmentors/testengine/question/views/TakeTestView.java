efd1abbf513a87afe76e2952fe28cd6d25566be1
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-07-17 14:43:27 +0530

    Code

---
+++ CodeG/Code1/src/com/brainmentors/testengine/question/views/TakeTestView.java
@@ -0,0 +1,49 @@
+package com.brainmentors.testengine.question.views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class TakeTestView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TakeTestView frame = new TakeTestView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TakeTestView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblStartTest = new JLabel("Start Test");
+		lblStartTest.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		lblStartTest.setBounds(153, 53, 113, 16);
+		contentPane.add(lblStartTest);
+	}
+
+}
---
+++ CodeG/Code1/src/com/brainmentors/testengine/util/constants/PathConstants.java
@@ -0,0 +1,6 @@
+package com.brainmentors.testengine.util.constants;
+
+public interface PathConstants {
+	String LOGIN_ICON ="/com/brainmentors/testengine/user/view/login_icon.png";
+	String UPLOAD_PATH = "/Users/amit/Documents/CoreJavaJuneBatch/TestEngine/uploads";
+}
